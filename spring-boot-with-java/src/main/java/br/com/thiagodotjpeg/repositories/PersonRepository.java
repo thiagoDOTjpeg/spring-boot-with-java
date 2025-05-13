d32f6a3ed7c4076aa57185f57c5b9d1464b54342
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2025-05-13 08:32:36 -0300

    test: added test for findbyname

--- spring-boot-with-java/src/main/java/br/com/thiagodotjpeg/repositories/PersonRepository.java
+++ spring-boot-with-java/src/main/java/br/com/thiagodotjpeg/repositories/PersonRepository.java
@@ -14,6 +14,6 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
   @Query("UPDATE Person p SET p.enabled = false WHERE p.id =:id")
   void disablePerson(@Param("id") Long id);
 
-  @Query("SELECT p FROM Person p WHERE p.firstName LIKE LOWER(CONCAT ('%',:firstName,'%'))")
+  @Query("SELECT p FROM Person p WHERE p.firstName LIKE LOWER(CONCAT('%',:firstName,'%'))")
   Page<Person> findPeopleByName(@Param("firstName") String firstName, Pageable pageable);
 }
---
+++ spring-boot-with-java/src/test/java/br/com/thiagodotjpeg/repositories/PersonRepositoryTest.java
@@ -0,0 +1,68 @@
+package br.com.thiagodotjpeg.repositories;
+
+import br.com.thiagodotjpeg.integrationstest.testcontainers.AbstractIntegrationTest;
+import br.com.thiagodotjpeg.models.Person;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(properties = "server.port=8888")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class PersonRepositoryTest extends AbstractIntegrationTest {
+
+  @Autowired
+  PersonRepository repository;
+  private static Person person;
+
+  @BeforeAll
+  static void setUp() {
+    person = new Person();
+  }
+
+  @Test
+  @Order(1)
+  void findPeopleByName() {
+    Pageable pageable = PageRequest.of(0, 12, Sort.by(Sort.Direction.ASC, "firstName"));
+
+    person = repository.findPeopleByName("thia", pageable).getContent().get(1);
+    System.out.println(person);
+
+    assertNotNull(person);
+    assertNotNull(person.getId());
+    assertEquals("Thiago", person.getFirstName());
+    assertEquals("Gritti", person.getLastName());
+    assertEquals("Male", person.getGender());
+    assertTrue(person.getEnabled());
+  }
+
+  @Test
+  @Order(2)
+  void disablePerson() {
+    long id = person.getId();
+    repository.disablePerson(id);
+
+    var result = repository.findById(id);
+    person = result.get();
+
+
+    assertNotNull(person);
+    assertNotNull(person.getId());
+    assertEquals("Thiago", person.getFirstName());
+    assertEquals("Gritti", person.getLastName());
+    assertEquals("Male", person.getGender());
+    assertFalse(person.getEnabled());
+  }
+
+}
