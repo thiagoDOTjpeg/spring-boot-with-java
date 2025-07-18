3d029ea7d57415c2b70ea6dcb6fe9536ddc360b3
thiagodotjpeg <thiago.gritti12@gmail.com>
2025-07-18 06:33:13 -0300

    ci/cd: changed some thins

--- spring-boot-with-java/src/main/java/br/com/thiagodotjpeg/config/OpenApiConfig.java
+++ spring-boot-with-java/src/main/java/br/com/thiagodotjpeg/config/OpenApiConfig.java
@@ -1,19 +1,25 @@
 package br.com.thiagodotjpeg.config;
 
+import br.com.thiagodotjpeg.services.InstanceInformationService;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Autowired
+    InstanceInformationService instanceInformationService;
+
   @Bean
   OpenAPI customOpenAPI() {
+
     return new OpenAPI()
             .info(new Info()
-                    .title("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker")
+                    .title("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker V1 " + instanceInformationService.retrieveInstanceInfo())
                     .version("v1")
                     .description("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker")
                     .termsOfService("https://github.com/thiagodotjpeg")
